82d4c1be9db69df478d7ed0b6ffbfcf35f96314f
Ragnar Rova <ragnar.rova@gmail.com>
2022-09-06 12:02:42 +0200

    Initial Commit

---
+++ GitlabNotifier/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitlabNotifier
+//
+//  Created by rrj on 2022-09-06.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
